5cd6b7877ad06f021d982360cb9d6a823814601d
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-04-01 22:56:38 +0800

    戴着铁砧锤时，可以查看红石粉和红石比较器的能量强度和模式
    实现了#1345中的#720部分

---
+++ src/main/java/dev/dubhe/anvilcraft/api/injections/tooltip/IInjectedTooltipProducer.java
@@ -0,0 +1,31 @@
+package dev.dubhe.anvilcraft.api.injections.tooltip;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+/**
+ * 头戴铁砧锤时需要显示tooltip，用于接口注入至原版内容
+ */
+@MethodsReturnNonnullByDefault
+public interface IInjectedTooltipProducer {
+    /**
+     * 获取需要渲染的tooltip
+     * @return 一个按顺序存储了所有需要渲染的tooltip的List
+     * @see IInjectedTooltipProducer#anvilcraft$getTooltip(BlockState)
+     */
+    default List<Component> anvilcraft$getTooltip() {
+        return List.of();
+    }
+
+    /**
+     * （用于Block）获取需要渲染的tooltip
+     * @return 一个按顺序存储了所有需要渲染的tooltip的List
+     * @see IInjectedTooltipProducer#anvilcraft$getTooltip()
+     */
+    default List<Component> anvilcraft$getTooltip(BlockState state) {
+        return List.of();
+    }
+}
