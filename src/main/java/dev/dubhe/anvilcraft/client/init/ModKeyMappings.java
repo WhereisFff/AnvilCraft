11a032b3f312f68321e5bb8d0764c1811d266f99
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-04-27 19:28:07 +0800

    添加更通用的KeyMappings注册

--- src/main/java/dev/dubhe/anvilcraft/client/init/ModKeyMappings.java
+++ src/main/java/dev/dubhe/anvilcraft/client/init/ModKeyMappings.java
@@ -8,13 +8,16 @@ import net.neoforged.neoforge.common.util.Lazy;
 import org.lwjgl.glfw.GLFW;
 
 public class ModKeyMappings {
-    public static final Lazy<KeyMapping> SWITCH_PHASE = Lazy.of(() -> new KeyMapping(
-        "key.anvilcraft.change_enchantment_space",
+    public static final Lazy<KeyMapping> SWITCH_PHASE = register(
+        "change_enchantment_space",
         KeyConflictContext.IN_GAME,
         InputConstants.Type.KEYSYM,
-        GLFW.GLFW_KEY_X,
-        "key.categories.anvilcraft"
-    ));
+        GLFW.GLFW_KEY_X
+    );
+
+    private static Lazy<KeyMapping> register(String name, KeyConflictContext context, InputConstants.Type type, int key) {
+        return Lazy.of(() -> new KeyMapping("key.anvilcraft." + name, context, type, key, "key.categories.anvilcraft"));
+    }
 
     public static void register(RegisterKeyMappingsEvent event) {
         event.register(SWITCH_PHASE.get());
