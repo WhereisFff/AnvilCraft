3c14fed9ebe834528d2609b428e323750d70dcbb
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-05-11 15:27:16 +0800

    补回遗漏的替换BlockItem部分

--- src/main/java/dev/dubhe/anvilcraft/init/ModBlocks.java
+++ src/main/java/dev/dubhe/anvilcraft/init/ModBlocks.java
@@ -124,6 +124,7 @@ import dev.dubhe.anvilcraft.block.state.DirectionCube3x3PartHalf;
 import dev.dubhe.anvilcraft.block.state.Vertical3PartHalf;
 import dev.dubhe.anvilcraft.block.state.Vertical4PartHalf;
 import dev.dubhe.anvilcraft.data.AnvilCraftDatagen;
+import dev.dubhe.anvilcraft.item.ChuteBlockItem;
 import dev.dubhe.anvilcraft.item.CursedBlockItem;
 import dev.dubhe.anvilcraft.item.EndDustBlockItem;
 import dev.dubhe.anvilcraft.item.FlexibleMultiPartBlockItem;
@@ -1322,7 +1323,7 @@ public class ModBlocks {
         .initialProperties(() -> Blocks.IRON_BLOCK)
         .properties(BlockBehaviour.Properties::noOcclusion)
         .blockstate(BlockStatProviderUtil::none)
-        .item(BlockItem::new)
+        .item(ChuteBlockItem::new)
         .onRegister(blockItem -> Item.BY_BLOCK.put(ModBlocks.SIMPLE_CHUTE.get(), blockItem))
         .build()
         .tag(BlockTags.MINEABLE_WITH_PICKAXE)
@@ -1343,7 +1344,7 @@ public class ModBlocks {
         .initialProperties(ModBlocks.CHUTE)
         .properties(BlockBehaviour.Properties::noOcclusion)
         .blockstate(BlockStatProviderUtil::none)
-        .item(BlockItem::new)
+        .item(ChuteBlockItem::new)
         .build()
         .tag(BlockTags.MINEABLE_WITH_PICKAXE)
         .recipe((ctx, provider) -> {
