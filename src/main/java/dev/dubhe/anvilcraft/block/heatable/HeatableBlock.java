65e66433de1798d9c5b9e826c77fe65ea656f3c8
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-06-24 20:49:13 +0800

    使可加热方块可推

--- src/main/java/dev/dubhe/anvilcraft/block/heatable/HeatableBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/heatable/HeatableBlock.java
@@ -2,17 +2,20 @@ package dev.dubhe.anvilcraft.block.heatable;
 
 import dev.dubhe.anvilcraft.api.heat.HeaterManager;
 import dev.dubhe.anvilcraft.block.entity.heatable.HeatableBlockEntity;
+import dev.dubhe.anvilcraft.block.piston.IMoveableEntityBlock;
+import dev.dubhe.anvilcraft.util.Util;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public abstract class HeatableBlock extends Block implements EntityBlock {
+public abstract class HeatableBlock extends Block implements IMoveableEntityBlock {
     protected HeatableBlock(Properties properties) {
         super(properties);
     }
@@ -31,5 +34,19 @@ public abstract class HeatableBlock extends Block implements EntityBlock {
             return (level1, pos, state1, blockEntity) -> HeatableBlockEntity.tick(level1, pos);
         }
         return null;
+    }
+
+    @Override
+    public CompoundTag clearData(Level level, BlockPos pos) {
+        CompoundTag nbt = new CompoundTag();
+        Util.castSafely(level.getBlockEntity(pos), HeatableBlockEntity.class)
+            .ifPresent(entity -> nbt.putInt("duration", entity.getDuration()));
+        return nbt;
+    }
+
+    @Override
+    public void setData(@NotNull Level level, @NotNull BlockPos pos, @NotNull CompoundTag nbt) {
+        Util.castSafely(level.getBlockEntity(pos), HeatableBlockEntity.class)
+            .ifPresent(entity -> entity.setDuration(nbt.getInt("duration")));
     }
 }
