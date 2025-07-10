12a6c5e3a5b46e9c5f22dc4b6b7bbece78fa3e8b
burin <ljx33071287@163.com>
2025-07-10 20:54:05 +0800

    Check style.

--- src/main/java/dev/dubhe/anvilcraft/mixin/ItemMixin.java
+++ src/main/java/dev/dubhe/anvilcraft/mixin/ItemMixin.java
@@ -33,38 +33,38 @@ public class ItemMixin {
     @Unique
     private final Map<ResourceKey<Enchantment>, Item> anvilcraft$enchantmentMappings = new Object2ObjectOpenHashMap<>() {
         {
-            put(Enchantments.SOUL_SPEED, Items.SNOUT_ARMOR_TRIM_SMITHING_TEMPLATE);// 灵魂疾行 猪鼻盔甲纹饰
-            put(Enchantments.FIRE_PROTECTION, Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE);// 火焰保护 肋骨盔甲纹饰
-            put(Enchantments.FIRE_ASPECT, Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE);// 火焰附加 肋骨盔甲纹饰
-            put(Enchantments.FLAME, Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE);// 火矢 肋骨盔甲纹饰
-            put(Enchantments.BLAST_PROTECTION, Items.DUNE_ARMOR_TRIM_SMITHING_TEMPLATE);// 爆炸保护 沙丘盔甲纹饰
-            put(Enchantments.SWIFT_SNEAK, Items.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE);// 迅捷潜行 幽静盔甲纹饰
-            put(Enchantments.PROTECTION, Items.WARD_ARMOR_TRIM_SMITHING_TEMPLATE);// 保护 监守盔甲纹饰
-            put(Enchantments.MENDING, Items.VEX_ARMOR_TRIM_SMITHING_TEMPLATE);// 经验修补 恼鬼盔甲纹饰
-            put(Enchantments.INFINITY, Items.SENTRY_ARMOR_TRIM_SMITHING_TEMPLATE);// 无限 哨兵盔甲纹饰
-            put(Enchantments.DENSITY, Items.BOLT_ARMOR_TRIM_SMITHING_TEMPLATE);// 致密 镶铆盔甲纹饰
-            put(Enchantments.BREACH, Items.BOLT_ARMOR_TRIM_SMITHING_TEMPLATE);// 破甲 镶铆盔甲纹饰
-            put(Enchantments.WIND_BURST, Items.FLOW_ARMOR_TRIM_SMITHING_TEMPLATE);// 风暴 涡流盔甲纹饰
-            put(Enchantments.PROJECTILE_PROTECTION, Items.WILD_ARMOR_TRIM_SMITHING_TEMPLATE);// 弹射物保护 荒野盔甲纹饰
-            put(Enchantments.FORTUNE, Items.SPIRE_ARMOR_TRIM_SMITHING_TEMPLATE);// 时运 尖塔盔甲纹饰
-            put(Enchantments.LOOTING, Items.EYE_ARMOR_TRIM_SMITHING_TEMPLATE);// 抢夺 眼眸盔甲纹饰
-            put(Enchantments.LUCK_OF_THE_SEA, Items.COAST_ARMOR_TRIM_SMITHING_TEMPLATE);// 海之眷顾 海岸盔甲纹饰
-            put(Enchantments.LURE, Items.COAST_ARMOR_TRIM_SMITHING_TEMPLATE);// 饵钓 海岸盔甲纹饰
-            put(Enchantments.DEPTH_STRIDER, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);// 深海探索者 潮汐盔甲纹饰
-            put(Enchantments.RESPIRATION, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);// 水下呼吸 潮汐盔甲纹饰
-            put(Enchantments.AQUA_AFFINITY, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);// 水下速掘 潮汐盔甲纹饰
-            put(Enchantments.IMPALING, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);// 穿刺 潮汐盔甲纹饰
-            put(Enchantments.RIPTIDE, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);// 激流 潮汐盔甲纹饰
+            put(Enchantments.SOUL_SPEED, Items.SNOUT_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.FIRE_PROTECTION, Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.FIRE_ASPECT, Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.FLAME, Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.BLAST_PROTECTION, Items.DUNE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.SWIFT_SNEAK, Items.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.PROTECTION, Items.WARD_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.MENDING, Items.VEX_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.INFINITY, Items.SENTRY_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.DENSITY, Items.BOLT_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.BREACH, Items.BOLT_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.WIND_BURST, Items.FLOW_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.PROJECTILE_PROTECTION, Items.WILD_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.FORTUNE, Items.SPIRE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.LOOTING, Items.EYE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.LUCK_OF_THE_SEA, Items.COAST_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.LURE, Items.COAST_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.DEPTH_STRIDER, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.RESPIRATION, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.AQUA_AFFINITY, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.IMPALING, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);
+            put(Enchantments.RIPTIDE, Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE);
         }
     };
 
     @Unique
     private final List<Item> anvilcraft$otherTemplate = new ArrayList<>() {
         {
-            add(Items.WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE);// 向导盔甲纹饰
-            add(Items.RAISER_ARMOR_TRIM_SMITHING_TEMPLATE);// 牧民盔甲纹饰
-            add(Items.HOST_ARMOR_TRIM_SMITHING_TEMPLATE);// 雇主盔甲纹饰
-            add(Items.SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE);// 塑造盔甲纹饰
+            add(Items.WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE);
+            add(Items.RAISER_ARMOR_TRIM_SMITHING_TEMPLATE);
+            add(Items.HOST_ARMOR_TRIM_SMITHING_TEMPLATE);
+            add(Items.SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE);
         }
     };
 
