77af5a02cd8ec66d43e42661db5edc82196c569d
TBPig <18257908603@163.com>
2025-07-17 14:12:32 +0800

    手册：添加空间超压器

--- src/main/java/dev/dubhe/anvilcraft/integration/patchouli/PatchouliIntegration.java
+++ src/main/java/dev/dubhe/anvilcraft/integration/patchouli/PatchouliIntegration.java
@@ -2,11 +2,7 @@ package dev.dubhe.anvilcraft.integration.patchouli;
 
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.integration.Integration;
-import dev.dubhe.anvilcraft.integration.patchouli.page.PageJewelCrafting;
-import dev.dubhe.anvilcraft.integration.patchouli.page.PageMultipleToOneSmithing;
-import dev.dubhe.anvilcraft.integration.patchouli.page.PageSuperHeating;
-import dev.dubhe.anvilcraft.integration.patchouli.page.PageTimeWarp;
-import vazkii.patchouli.api.VariableHelper;
+import dev.dubhe.anvilcraft.integration.patchouli.page.*;
 import vazkii.patchouli.client.book.ClientBookRegistry;
 
 @SuppressWarnings("unused")
@@ -20,5 +16,7 @@ public class PatchouliIntegration {
         ClientBookRegistry.INSTANCE.pageTypes.put(AnvilCraft.of("super_heating"), PageSuperHeating.class);
         ClientBookRegistry.INSTANCE.pageTypes.put(AnvilCraft.of("jewel_crafting"), PageJewelCrafting.class);
         ClientBookRegistry.INSTANCE.pageTypes.put(AnvilCraft.of("multiple_to_one_smithing"), PageMultipleToOneSmithing.class);
+        ClientBookRegistry.INSTANCE.pageTypes.put(AnvilCraft.of("item_inject"), PageItemInject.class);
+        ClientBookRegistry.INSTANCE.pageTypes.put(AnvilCraft.of("block_compress"), PageBlockCompress.class);
     }
 }
---
+++ src/main/java/dev/dubhe/anvilcraft/integration/patchouli/page/PageItemInject.java
@@ -0,0 +1,60 @@
+package dev.dubhe.anvilcraft.integration.patchouli.page;
+
+import dev.dubhe.anvilcraft.init.ModRecipeTypes;
+import dev.dubhe.anvilcraft.integration.patchouli.util.PatchouliRenderHelper;
+import dev.dubhe.anvilcraft.recipe.anvil.ItemInjectRecipe;
+import dev.dubhe.anvilcraft.util.RenderHelper;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import vazkii.patchouli.client.book.gui.GuiBook;
+import vazkii.patchouli.client.book.page.abstr.PageDoubleRecipeRegistry;
+
+import java.util.List;
+
+public class PageItemInject extends PageDoubleRecipeRegistry<ItemInjectRecipe> {
+    public PageItemInject() {super(ModRecipeTypes.ITEM_INJECT_TYPE.get());}
+
+    @Override
+    protected void drawRecipe(
+            GuiGraphics graphics, ItemInjectRecipe recipe, int recipeX, int recipeY, int mouseX, int mouseY, boolean second
+    ) {
+        PatchouliRenderHelper.render1x1(graphics, recipeX - 4, recipeY + 16);
+
+        List<Object2IntMap.Entry<Ingredient>> ingredients = recipe.getMergedIngredients();
+        PatchouliRenderHelper.renderIngredientWithCount(parent, graphics, ingredients.get(0), recipeX, recipeY + 20, mouseX, mouseY);
+
+        PatchouliRenderHelper.renderArray(graphics, recipeX + 25, recipeY + 20);
+
+        PatchouliRenderHelper.renderAnvilWithAnimation(parent,graphics,recipeX + 50, recipeY + 15);
+
+        RenderHelper.renderBlock(graphics,recipe.inputBlock.defaultBlockState(),
+                recipeX + 50, recipeY + 31, 0,
+                12,
+                RenderHelper.SINGLE_BLOCK);
+
+        PatchouliRenderHelper.renderArray(graphics, recipeX + 66, recipeY + 20);
+
+        RenderHelper.renderBlock(graphics,recipe.resultBlock.defaultBlockState(),
+                recipeX + 90, recipeY + 31, 0,
+                12,
+                RenderHelper.SINGLE_BLOCK);
+
+        parent.drawCenteredStringNoShadow(
+                graphics, getTitle(second).getVisualOrderText(),
+                GuiBook.PAGE_WIDTH / 2, recipeY - 5,
+                book.headerColor
+        );
+    }
+    @Override
+    protected ItemStack getRecipeOutput(Level level, ItemInjectRecipe recipe) {
+         return recipe.getResultItem(level.registryAccess());
+    }
+
+    @Override
+    protected int getRecipeHeight() {
+        return 78;
+    }
+}
