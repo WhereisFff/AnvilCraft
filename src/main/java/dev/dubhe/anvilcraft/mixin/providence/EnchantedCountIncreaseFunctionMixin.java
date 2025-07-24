64acd0c0501e70bcb5d28089661b66cab4d01df4
QiuShui0875 <qiushui1012@gmail.com>
2025-07-24 17:25:33 +0800

    修复所有工具都能强运的问题-2

--- src/main/java/dev/dubhe/anvilcraft/mixin/providence/EnchantedCountIncreaseFunctionMixin.java
+++ src/main/java/dev/dubhe/anvilcraft/mixin/providence/EnchantedCountIncreaseFunctionMixin.java
@@ -2,11 +2,13 @@ package dev.dubhe.anvilcraft.mixin.providence;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.dubhe.anvilcraft.init.ModComponents;
 import dev.dubhe.anvilcraft.init.ModEnchantmentTags;
 import net.minecraft.core.Holder;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.functions.EnchantedCountIncreaseFunction;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,7 +27,10 @@ public class EnchantedCountIncreaseFunctionMixin {
                      + "getFloat(Lnet/minecraft/world/level/storage/loot/LootContext;)F"))
     private float getMultipleForProvidence(NumberProvider instance, LootContext context, Operation<Float> original) {
         float result = original.call(instance, context);
-        if (!this.enchantment.is(ModEnchantmentTags.PROVIDENCE_BONUS)) return result;
+        if (!(context.getParamOrNull(LootContextParams.TOOL) instanceof ItemStack stack)
+            || !stack.has(ModComponents.PROVIDENCE)
+            || !this.enchantment.is(ModEnchantmentTags.PROVIDENCE_BONUS)
+        ) return result;
         float random = context.getRandom().nextFloat();
         if (random >= 0.25f) return result;
         result += original.call(instance, context);
