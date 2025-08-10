fcc0e2150050dfb4368dbc12d2059dc0f6262b05
burin <ljx33071287@163.com>
2025-08-10 17:04:10 +0800

    feat(AnvilHitGunpowderBlockEventListener,AnvilHitSugarBlockEventListener): Removed the event listener of the giant anvil falling to the ground.

--- src/main/java/dev/dubhe/anvilcraft/event/anvil/AnvilHitGunpowderBlockEventListener.java
+++ src/main/java/dev/dubhe/anvilcraft/event/anvil/AnvilHitGunpowderBlockEventListener.java
@@ -2,7 +2,6 @@ package dev.dubhe.anvilcraft.event.anvil;
 
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.event.anvil.AnvilEvent;
-import dev.dubhe.anvilcraft.api.event.anvil.GiantAnvilFallOnLandEvent;
 import dev.dubhe.anvilcraft.block.GunpowderBlock;
 import dev.dubhe.anvilcraft.entity.AnimateAscendingBlockEntity;
 import net.minecraft.MethodsReturnNonnullByDefault;
@@ -21,30 +20,6 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class AnvilHitGunpowderBlockEventListener {
     @SubscribeEvent
     public static void onLand(AnvilEvent.OnLand event) {
-        Level level = event.getLevel();
-        final BlockPos pos = event.getPos();
-        final BlockState blockState = level.getBlockState(pos);
-        final BlockPos hitPos = pos.below();
-        final BlockState hitState = level.getBlockState(hitPos);
-        if (hitState.getBlock() instanceof GunpowderBlock gunpowderBlock) {
-            gunpowderBlock.explosion(level, hitPos);
-            int distance = (int) Math.ceil(event.getFallDistance()) + 1;
-            BlockPos above = pos;
-            for (int i = 1; i < distance + 1; i++) {
-                above = above.above();
-                if (!level.getBlockState(above).isAir()) {
-                    break;
-                }
-            }
-            above = above.below();
-            level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
-            AnimateAscendingBlockEntity.animate(level, pos, blockState, above);
-            level.setBlockAndUpdate(above, blockState);
-        }
-    }
-
-    @SubscribeEvent
-    public static void giantAnvilOnLand(GiantAnvilFallOnLandEvent event) {
         Level level = event.getLevel();
         final BlockPos pos = event.getPos();
         final BlockState blockState = level.getBlockState(pos);
