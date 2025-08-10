d125c391d0f7adc767bf2847a82a6ac85ba81671
burin <ljx33071287@163.com>
2025-08-10 18:16:42 +0800

    feat(AnvilHitGunpowderBlockEventListener): 添加铁砧锤砸火药块的事件处理和阻止大铁砧砸火药块的事件

--- src/main/java/dev/dubhe/anvilcraft/event/anvil/AnvilHitGunpowderBlockEventListener.java
+++ src/main/java/dev/dubhe/anvilcraft/event/anvil/AnvilHitGunpowderBlockEventListener.java
@@ -2,8 +2,10 @@ package dev.dubhe.anvilcraft.event.anvil;
 
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.event.anvil.AnvilEvent;
+import dev.dubhe.anvilcraft.api.event.anvil.AnvilFallOnLandEvent;
 import dev.dubhe.anvilcraft.block.GunpowderBlock;
 import dev.dubhe.anvilcraft.entity.AnimateAscendingBlockEntity;
+import dev.dubhe.anvilcraft.init.ModBlocks;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
@@ -26,6 +28,33 @@ public class AnvilHitGunpowderBlockEventListener {
         final BlockPos hitPos = pos.below();
         final BlockState hitState = level.getBlockState(hitPos);
         if (hitState.getBlock() instanceof GunpowderBlock gunpowderBlock) {
+            gunpowderBlock.explosion(level, hitPos);
+            int distance = (int) Math.ceil(event.getFallDistance()) + 1;
+            BlockPos above = pos;
+            for (int i = 1; i < distance + 1; i++) {
+                above = above.above();
+                if (!level.getBlockState(above).isAir()) {
+                    break;
+                }
+            }
+            above = above.below();
+            level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+            AnimateAscendingBlockEntity.animate(level, pos, blockState, above);
+            level.setBlockAndUpdate(above, blockState);
+        }
+    }
+
+    @SubscribeEvent
+    public static void anvilHammerHit(AnvilFallOnLandEvent event) {
+        Level level = event.getLevel();
+        final BlockPos pos = event.getPos();
+        final BlockState blockState = level.getBlockState(pos);
+        final BlockPos hitPos = pos.below();
+        final BlockState hitState = level.getBlockState(hitPos);
+        if (hitState.getBlock() instanceof GunpowderBlock gunpowderBlock) {
+            if (blockState.is(ModBlocks.GIANT_ANVIL)) {
+                return;
+            }
             gunpowderBlock.explosion(level, hitPos);
             int distance = (int) Math.ceil(event.getFallDistance()) + 1;
             BlockPos above = pos;
