257b5baecaba186e069e9e0e16927fed15984b33
YocyCraft <hanhengrui@163.com>
2025-08-13 22:15:42 +0800

    Implement features

--- src/main/java/dev/dubhe/anvilcraft/block/heatable/HeatableBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/heatable/HeatableBlock.java
@@ -3,7 +3,6 @@ package dev.dubhe.anvilcraft.block.heatable;
 import dev.dubhe.anvilcraft.api.heat.HeatRecorder;
 import dev.dubhe.anvilcraft.api.heat.HeaterManager;
 import dev.dubhe.anvilcraft.block.entity.heatable.HeatableBlockEntity;
-import dev.dubhe.anvilcraft.block.entity.heatable.OverheatedBlockEntity;
 import dev.dubhe.anvilcraft.util.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
@@ -11,6 +10,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.TntBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -60,9 +61,20 @@ public abstract class HeatableBlock extends Block {
     public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
         if (placer instanceof ServerPlayer player && player.gameMode.isCreative()) {
             Optional.ofNullable(level.getBlockEntity(pos))
-            .filter(OverheatedBlockEntity.class::isInstance)
-            .map(OverheatedBlockEntity.class::cast)
-            .ifPresent(be -> be.setDuration(1200));
+                .filter(HeatableBlockEntity.class::isInstance)
+                .map(HeatableBlockEntity.class::cast)
+                .ifPresent(be -> be.setDuration(1200));
         }
+        super.setPlacedBy(level, pos, state, placer, stack);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
+        if (level.getBlockState(neighborPos).is(Blocks.TNT)) {
+            TntBlock.explode(level, neighborPos);
+            level.removeBlock(neighborPos, false);
+        }
+        super.neighborChanged(state, level, pos, neighborBlock, neighborPos, movedByPiston);
     }
 }
