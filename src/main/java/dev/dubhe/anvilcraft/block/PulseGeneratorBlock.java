04b37e77e5c9c31f825712c51e1f05d8d960d2f2
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-08-15 15:12:36 +0800

    重构脉冲发生器

--- src/main/java/dev/dubhe/anvilcraft/block/PulseGeneratorBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/PulseGeneratorBlock.java
@@ -1,6 +1,7 @@
 package dev.dubhe.anvilcraft.block;
 
 import com.mojang.serialization.MapCodec;
+import dev.dubhe.anvilcraft.api.hammer.IHammerChangeable;
 import dev.dubhe.anvilcraft.api.hammer.IHammerRemovable;
 import dev.dubhe.anvilcraft.block.entity.PulseGeneratorBlockEntity;
 import dev.dubhe.anvilcraft.init.ModBlockEntities;
@@ -8,6 +9,7 @@ import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.DustParticleOptions;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
@@ -18,31 +20,31 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityTicker;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.event.EventHooks;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.EnumSet;
+import java.util.Optional;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
-public class PulseGeneratorBlock extends HorizontalDirectionalBlock implements EntityBlock, IHammerRemovable {
+public class PulseGeneratorBlock extends HorizontalDirectionalBlock implements EntityBlock, IHammerChangeable, IHammerRemovable {
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
     protected static final VoxelShape SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 4.0, 16.0);
     public static final MapCodec<PulseGeneratorBlock> CODEC = simpleCodec(PulseGeneratorBlock::new);
@@ -128,16 +130,92 @@ public class PulseGeneratorBlock extends HorizontalDirectionalBlock implements E
         level.updateNeighborsAtExceptFromFacing(front, this, facing);
     }
 
-    protected void update(Level level, BlockPos pos, BlockState state) {
+    public void update(Level level, BlockPos pos, BlockState state) {
         if (level.isClientSide) return;
 
         BlockEntity blockentity = level.getBlockEntity(pos);
         if (!(blockentity instanceof PulseGeneratorBlockEntity generator)) return;
-        boolean nowInputting = getInputSignal(level, pos, state) > 0;
+        boolean lastInputting = generator.isInputtingSignal();
+        boolean nowInputting = PulseGeneratorBlock.getInputSignal(level, pos, state) > 0;
+        generator.setInputtingSignal(nowInputting);
 
-        if (PulseGeneratorBlockEntity.canStart(generator, nowInputting)) {
-            generator.start(level, pos, state);
+        boolean canStart = switch (generator.getStartMode()) {
+            case RISING_EDGE -> !lastInputting && nowInputting;
+            case FALLING_EDGE -> lastInputting && !nowInputting;
+            case LOOP -> !generator.isDeadlock() && generator.getState() == PulseGeneratorBlockEntity.State.DEFAULT;
+        } && !generator.isProcessing();
+        if (canStart) {
+            this.startWaiting(level, pos, state, generator);
         }
+
+        this.checkIsDeadlock(level, pos, state, generator);
+        this.updateBlockAndNeighbours(level, pos, state, generator);
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        Optional<PulseGeneratorBlockEntity> generatorOp = level.getBlockEntity(pos, ModBlockEntities.PULSE_GENERATOR.get());
+        if (generatorOp.isEmpty()) return;
+        PulseGeneratorBlockEntity generator = generatorOp.get();
+        if (!generator.isDeadlock()) switch (generator.getState()) {
+            case WAITING -> this.startOutputting(level, pos, state, generator);
+            case OUTPUTTING -> this.checkOnSignalEnd(level, pos, state, generator);
+        }
+    }
+
+    protected void checkIsDeadlock(Level level, BlockPos pos, BlockState state, PulseGeneratorBlockEntity generator) {
+        if (generator.getStartMode() == PulseGeneratorBlockEntity.Mode.LOOP) {
+            if (generator.isDeadlock() && !generator.isInputtingSignal()) {
+                this.startWaiting(level, pos, state, generator);
+                generator.setDeadlock(false);
+            } else {
+                generator.setDeadlock(generator.isInputtingSignal());
+            }
+        }
+        if (generator.isDeadlock()) {
+            generator.setState(PulseGeneratorBlockEntity.State.DEFAULT);
+            this.updateBlockAndNeighbours(level, pos, state, generator);
+        }
+    }
+
+    public void startWaiting(Level level, BlockPos pos, BlockState state, PulseGeneratorBlockEntity generator) {
+        generator.setState(PulseGeneratorBlockEntity.State.WAITING);
+        if (generator.getWaitingTime() != 0) {
+            level.scheduleTick(pos, this, generator.getWaitingTime());
+        } else {
+            this.startOutputting(level, pos, state, generator);
+        }
+    }
+
+    protected void startOutputting(Level level, BlockPos pos, BlockState state, PulseGeneratorBlockEntity generator) {
+        generator.setState(PulseGeneratorBlockEntity.State.OUTPUTTING);
+        if (generator.getSignalDuration() == 0) {
+            this.updateBlockAndNeighbours(level, pos, state, generator);
+            this.checkOnSignalEnd(level, pos, state, generator);
+            return;
+        }
+        level.scheduleTick(pos, this, generator.getSignalDuration());
+        this.updateBlockAndNeighbours(level, pos, state, generator);
+    }
+
+    protected void checkOnSignalEnd(Level level, BlockPos pos, BlockState state, PulseGeneratorBlockEntity generator) {
+        generator.setState(PulseGeneratorBlockEntity.State.DEFAULT);
+        this.updateBlockAndNeighbours(level, pos, state, generator);
+
+        if (generator.getStartMode() == PulseGeneratorBlockEntity.Mode.LOOP) {
+            this.startWaiting(level, pos, state, generator);
+        }
+    }
+
+    protected void updateBlockAndNeighbours(Level level, BlockPos pos, BlockState state, PulseGeneratorBlockEntity generator) {
+        boolean powered = state.getValue(POWERED);
+        boolean shouldPower = generator.isOutputting();
+        if (powered == shouldPower) return;
+        Direction direction = state.getValue(FACING).getOpposite();
+        BlockPos neighbourPos = pos.relative(direction);
+        level.setBlockAndUpdate(pos, state.setValue(POWERED, shouldPower));
+        level.neighborChanged(neighbourPos, state.getBlock(), pos);
+        level.updateNeighborsAtExceptFromFacing(neighbourPos, state.getBlock(), direction.getOpposite());
     }
 
     @Override
@@ -152,17 +230,6 @@ public class PulseGeneratorBlock extends HorizontalDirectionalBlock implements E
             double d5 = direction.getStepZ() * d3;
             level.addParticle(DustParticleOptions.REDSTONE, d0 + d4, d1, d2 + d5, 0.0, 0.0, 0.0);
         }
-    }
-
-    @Override
-    public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(
-        Level level, BlockState state,
-        BlockEntityType<T> type
-    ) {
-        if (level.isClientSide()) return null;
-        if (type != ModBlockEntities.PULSE_GENERATOR.get()) return null;
-        return (level1, pos, state1, blockEntity) ->
-            PulseGeneratorBlockEntity.tick(level1, pos, state1, (PulseGeneratorBlockEntity) blockEntity);
     }
 
     public static int getInputSignal(Level level, BlockPos pos, BlockState state) {
@@ -203,5 +270,17 @@ public class PulseGeneratorBlock extends HorizontalDirectionalBlock implements E
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING, POWERED);
+    }
+
+    @Override
+    public boolean change(Player player, BlockPos blockPos, @NotNull Level level, ItemStack anvilHammer) {
+        BlockState bs = level.getBlockState(blockPos);
+        level.setBlockAndUpdate(blockPos, bs.cycle(FACING));
+        return true;
+    }
+
+    @Override
+    public @Nullable Property<?> getChangeableProperty(BlockState blockState) {
+        return FACING;
     }
 }
