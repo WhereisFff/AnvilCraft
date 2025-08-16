40bcc17ccaa0e38e3ea2611f50e90e5597b752ea
Abslb <2427310499@qq.com>
2025-08-16 19:04:46 +0100

    Added extra checkings to avoid non-expected warning skip.

--- src/main/java/dev/dubhe/anvilcraft/mixin/ServerLevelMixin.java
+++ src/main/java/dev/dubhe/anvilcraft/mixin/ServerLevelMixin.java
@@ -16,13 +16,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ServerLevelMixin {
     @Unique
     ItemEntity anvilcraft$addedEntity;
+    @Unique
+    boolean anvilcraft$shouldCheckDiscarded;
 
     @Inject(method = "addEntity", at = @At(
         value = "INVOKE",
         target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V")
     )
     public void recordAddedItemEntity(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (entity instanceof ItemEntity e1) anvilcraft$addedEntity = e1;
+        if (entity instanceof ItemEntity e1) {
+            anvilcraft$addedEntity = e1;
+            anvilcraft$shouldCheckDiscarded = true;
+        }
     }
 
     @Redirect(method = "addEntity", at = @At(
@@ -30,8 +35,13 @@ public class ServerLevelMixin {
         target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V")
     )
     public void cancelItemDiscardedWarn(Logger instance, String s, Object o) {
-        if (IDiscardableItemEntity.castFromItemEntity(anvilcraft$addedEntity).anvilcraft$getDiscarded())
+        if (anvilcraft$shouldCheckDiscarded
+            && IDiscardableItemEntity
+            .castFromItemEntity(anvilcraft$addedEntity).anvilcraft$getDiscarded()) {
+            anvilcraft$shouldCheckDiscarded = false;
             return;
+        }
+
         instance.warn(s, o);
     }
 }
