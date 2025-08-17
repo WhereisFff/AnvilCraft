4a1b68a1ffb4fd1c155f817af071192152fa4bea
Gugle <administration@ztxy666.cn>
2025-08-18 06:16:25 +0800

    feat(ponder): 修改磁铁块相关教程并优化标签注册
    - 修改磁铁块教程中部分文本及其显示位置
    - 优化铁砧标签注册，使其支持所有铁砧变种

--- src/main/java/dev/dubhe/anvilcraft/integration/ponder/AnvilCraftPonderTags.java
+++ src/main/java/dev/dubhe/anvilcraft/integration/ponder/AnvilCraftPonderTags.java
@@ -4,28 +4,50 @@ import com.tterrag.registrate.util.entry.RegistryEntry;
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.init.ModBlocks;
 import net.createmod.ponder.api.registration.PonderTagRegistrationHelper;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 public class AnvilCraftPonderTags {
     public static final ResourceLocation ANVIL = AnvilCraft.of("anvil");
+    public static final ResourceLocation MAGNET_BLOCK = AnvilCraft.of("magnet_block");
 
     public static void register(@NotNull PonderTagRegistrationHelper<ResourceLocation> helper) {
-        PonderTagRegistrationHelper<RegistryEntry<?, ?>> tagHelper = helper.withKeyFunction(RegistryEntry::getId);
+        PonderTagRegistrationHelper<RegistryEntry<?, ?>> registryTagHelper = helper.withKeyFunction(RegistryEntry::getId);
+        PonderTagRegistrationHelper<Item> itemTagHelper = helper.withKeyFunction(BuiltInRegistries.ITEM::getKey);
 
-        tagHelper.registerTag(ANVIL)
+        helper.registerTag(ANVIL)
             .addToIndex()
-            .item(Items.ANVIL)
-            .item(Items.CHIPPED_ANVIL)
-            .item(Items.DAMAGED_ANVIL)
-            .item(ModBlocks.EMBER_ANVIL)
-            .item(ModBlocks.GIANT_ANVIL)
-            .item(ModBlocks.ROYAL_ANVIL)
-            .item(ModBlocks.SPECTRAL_ANVIL)
-            .item(ModBlocks.TRANSCENDENCE_ANVIL)
+            .item(Items.ANVIL, true, false)
             .title("Anvil")
             .description("Use anvil to craft")
             .register();
+
+        helper.registerTag(MAGNET_BLOCK)
+            .addToIndex()
+            .item(ModBlocks.MAGNET_BLOCK, true, false)
+            .title("Magnet")
+            .description("Use magnet to attract the anvil")
+            .register();
+
+
+        itemTagHelper.addToTag(ANVIL)
+            .add(Items.ANVIL)
+            .add(Items.CHIPPED_ANVIL)
+            .add(Items.DAMAGED_ANVIL);
+
+        registryTagHelper.addToTag(ANVIL)
+            .add(ModBlocks.ROYAL_ANVIL)
+            .add(ModBlocks.EMBER_ANVIL)
+            .add(ModBlocks.TRANSCENDENCE_ANVIL)
+            .add(ModBlocks.SPECTRAL_ANVIL)
+            .add(ModBlocks.GIANT_ANVIL);
+
+        registryTagHelper.addToTag(MAGNET_BLOCK)
+            .add(ModBlocks.MAGNET_BLOCK)
+            .add(ModBlocks.HOLLOW_MAGNET_BLOCK)
+            .add(ModBlocks.FERRITE_CORE_MAGNET_BLOCK);
     }
 }
