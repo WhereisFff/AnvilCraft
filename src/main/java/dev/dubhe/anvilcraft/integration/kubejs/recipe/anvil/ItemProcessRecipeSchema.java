7b64d788c82d673d476da643e29e6c68ebe18bb1
Gugle <administration@ztxy666.cn>
2025-08-19 19:28:06 +0800

    refactor: 移除多余的重载方法
    - 删除了 ItemProcessRecipeBuilder 类中多个重载的 result 方法
    - 保留了最常用的两个方法签名：
      1. result(ItemStack result)
      2. result(ItemStack result, NumberProvider count)
    - 其他重载方法可以通过调用现有方法来实现相同的功能
    - 此更改简化了代码结构，减少了冗余的实现

--- src/main/java/dev/dubhe/anvilcraft/integration/kubejs/recipe/anvil/ItemProcessRecipeSchema.java
+++ src/main/java/dev/dubhe/anvilcraft/integration/kubejs/recipe/anvil/ItemProcessRecipeSchema.java
@@ -16,7 +16,6 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.storage.loot.providers.number.BinomialDistributionGenerator;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
 import org.jetbrains.annotations.NotNull;
@@ -59,31 +58,8 @@ public interface ItemProcessRecipeSchema {
             return this;
         }
 
-        public ItemProcessKubeRecipe result(@NotNull ItemStack result, float chance) {
-            return this.result(result, BinomialDistributionGenerator.binomial(result.getCount(), chance));
-        }
-
         public ItemProcessKubeRecipe result(@NotNull ItemStack result) {
             return this.result(result, ConstantValue.exactly(result.getCount()));
-        }
-
-        public ItemProcessKubeRecipe result(@NotNull ItemLike result, NumberProvider count) {
-            this.computeIfAbsent(RESULTS, ArrayList::new)
-                .add(ChanceItemStack.of(result, count));
-            this.save();
-            return this;
-        }
-
-        public ItemProcessKubeRecipe result(@NotNull ItemLike result, int count, float chance) {
-            return this.result(result, BinomialDistributionGenerator.binomial(count, chance));
-        }
-
-        public ItemProcessKubeRecipe result(@NotNull ItemLike result, int count) {
-            return this.result(result, ConstantValue.exactly(count));
-        }
-
-        public ItemProcessKubeRecipe result(@NotNull ItemLike result, float chance) {
-            return this.result(result, 1, chance);
         }
 
         @Override
