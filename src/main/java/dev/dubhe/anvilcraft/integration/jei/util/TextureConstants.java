353afbd98ede9424f29bf6e39adab2fc33002c10
theabab23333 <2047477146@qq.com>
2025-08-22 04:45:30 +0800

    添加GUI常用资源方法
    - 在 JeiRenderHelper 类中添加了多个获取箭头、槽位和其他 GUI元素的方法
    - 这些方法和常量便于在JEI集成中统一管理和使用GUI资源

--- src/main/java/dev/dubhe/anvilcraft/integration/jei/util/TextureConstants.java
+++ src/main/java/dev/dubhe/anvilcraft/integration/jei/util/TextureConstants.java
@@ -11,16 +11,19 @@ public class TextureConstants {
 
     public static final ResourceLocation ANVIL_CRAFT_SPRITES = AnvilCraft.of(BASE_PATH + "sprites/jei.png");
 
+    // Arrow
     public static final ResourceLocation ARROW_DEFAULT = AnvilCraft.of(BASE_PATH + "sprites/jei/arrow_default.png");
     public static final ResourceLocation ARROW_BLOCK_CONVERSION = AnvilCraft.of(BASE_PATH + "sprites/jei/block_conversion.png");
     public static final ResourceLocation ARROW_INPUT = AnvilCraft.of(BASE_PATH + "sprites/jei/input.png");
     public static final ResourceLocation ARROW_OUTPUT = AnvilCraft.of(BASE_PATH + "sprites/jei/output.png");
     public static final ResourceLocation ARROW_OUTPUT_FROM_BELOW = AnvilCraft.of(BASE_PATH + "sprites/jei/output_from_below.png");
 
+    // Slot
     public static final ResourceLocation SLOT_CHOICE = AnvilCraft.of(BASE_PATH + "sprites/jei/slot_choice.png");
     public static final ResourceLocation SLOT_DEFAULT = AnvilCraft.of(BASE_PATH + "sprites/jei/slot_default.png");
     public static final ResourceLocation SLOT_PROBABILITY = AnvilCraft.of(BASE_PATH + "sprites/jei/slot_probability.png");
 
+    // Other
     public static final ResourceLocation EXPLOSION = AnvilCraft.of(BASE_PATH + "sprites/jei/explosion.png");
 
 
