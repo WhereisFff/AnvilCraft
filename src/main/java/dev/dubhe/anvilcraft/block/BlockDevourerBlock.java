382863106bb1da3e6cb5264921aad892a6cc0a40
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-08-22 11:07:36 +0800

    修复大铁砧落地时触发的小铁砧落地事件的下落距离错误的问题

--- src/main/java/dev/dubhe/anvilcraft/block/BlockDevourerBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/BlockDevourerBlock.java
@@ -103,11 +103,13 @@ public class BlockDevourerBlock extends DirectionalBlock implements HammerRotate
     }
 
     @Override
-    protected void onPlace(BlockState state,
-                           Level level,
-                           BlockPos pos,
-                           BlockState oldState,
-                           boolean movedByPiston) {
+    protected void onPlace(
+        BlockState state,
+        Level level,
+        BlockPos pos,
+        BlockState oldState,
+        boolean movedByPiston
+    ) {
         if (!level.isClientSide) {
             checkIfTriggered(level, state, pos);
         }
@@ -118,7 +120,8 @@ public class BlockDevourerBlock extends DirectionalBlock implements HammerRotate
         BlockState state,
         ServerLevel level,
         BlockPos pos,
-        RandomSource random) {
+        RandomSource random
+    ) {
         super.tick(state, level, pos, random);
         if (!state.getValue(TRIGGERED)) return;
         if (!BlockPlacerBlock.hasNeighborSignal(level, pos, state.getValue(FACING))) {
@@ -133,7 +136,8 @@ public class BlockDevourerBlock extends DirectionalBlock implements HammerRotate
         BlockPos pos,
         Block neighborBlock,
         BlockPos neighborPos,
-        boolean movedByPiston) {
+        boolean movedByPiston
+    ) {
         if (!level.isClientSide) {
             checkIfTriggered(level, state, pos);
         }
@@ -160,7 +164,8 @@ public class BlockDevourerBlock extends DirectionalBlock implements HammerRotate
         BlockState state,
         BlockGetter level,
         BlockPos pos,
-        CollisionContext context) {
+        CollisionContext context
+    ) {
         return switch (state.getValue(FACING)) {
             case DOWN -> DOWN_SHAPE;
             case UP -> UP_SHAPE;
@@ -195,7 +200,8 @@ public class BlockDevourerBlock extends DirectionalBlock implements HammerRotate
         BlockPos devourerPos,
         Direction devourerDirection,
         int range,
-        @Nullable Block anvil) {
+        @Nullable Block anvil
+    ) {
         BlockPos outputPos = devourerPos.relative(devourerDirection.getOpposite());
         BlockPos devourCenterPos = devourerPos.relative(devourerDirection);
         final List<IItemHandler> itemHandlerList = getTargetItemHandlerList(
@@ -210,19 +216,22 @@ public class BlockDevourerBlock extends DirectionalBlock implements HammerRotate
             case DOWN, UP -> {
                 devourBlockPosList = BlockPos.betweenClosed(
                     devourCenterPos.relative(Direction.NORTH, range).relative(Direction.WEST, range),
-                    devourCenterPos.relative(Direction.SOUTH, range).relative(Direction.EAST, range));
+                    devourCenterPos.relative(Direction.SOUTH, range).relative(Direction.EAST, range)
+                );
                 maxY = devourCenterPos.getY();
             }
             case NORTH, SOUTH -> {
                 devourBlockPosList = BlockPos.betweenClosed(
                     devourCenterPos.relative(Direction.UP, range).relative(Direction.WEST, range),
-                    devourCenterPos.relative(Direction.DOWN, range).relative(Direction.EAST, range));
+                    devourCenterPos.relative(Direction.DOWN, range).relative(Direction.EAST, range)
+                );
                 maxY = devourCenterPos.relative(Direction.UP, range).getY();
             }
             case WEST, EAST -> {
                 devourBlockPosList = BlockPos.betweenClosed(
                     devourCenterPos.relative(Direction.UP, range).relative(Direction.NORTH, range),
-                    devourCenterPos.relative(Direction.DOWN, range).relative(Direction.SOUTH, range));
+                    devourCenterPos.relative(Direction.DOWN, range).relative(Direction.SOUTH, range)
+                );
                 maxY = devourCenterPos.relative(Direction.UP, range).getY();
             }
             default -> {
