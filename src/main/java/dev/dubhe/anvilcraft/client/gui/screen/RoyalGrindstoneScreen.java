76a8afedd796c2fdca28227a512d367bb93bae91
VictorModi <vmmba262@gmail.com>
2025-08-29 21:20:42 +0800

    feat: 使皇家砂轮可以接受金块

--- src/main/java/dev/dubhe/anvilcraft/client/gui/screen/RoyalGrindstoneScreen.java
+++ src/main/java/dev/dubhe/anvilcraft/client/gui/screen/RoyalGrindstoneScreen.java
@@ -1,13 +1,19 @@
 package dev.dubhe.anvilcraft.client.gui.screen;
 
+import com.mojang.datafixers.util.Pair;
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.inventory.RoyalGrindstoneMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class RoyalGrindstoneScreen extends AbstractContainerScreen<RoyalGrindstoneMenu> {
     private static final ResourceLocation GRINDSTONE_LOCATION =
@@ -35,12 +41,30 @@ public class RoyalGrindstoneScreen extends AbstractContainerScreen<RoyalGrindsto
         int i = (this.width - this.imageWidth) / 2;
         int j = (this.height - this.imageHeight) / 2;
         guiGraphics.blit(GRINDSTONE_LOCATION, i, j, 0, 0, this.imageWidth, this.imageHeight);
-        guiGraphics.setColor(1, 1, 1, 0.5F);
-        guiGraphics.renderItem(
-            RoyalGrindstoneMenu.REPAIR_MATERIAL.getDefaultInstance(), i + 89, j + 22, (int) (partialTick * 100));
-        guiGraphics.renderItem(
-            RoyalGrindstoneMenu.RESULT_MATERIAL.getDefaultInstance(), i + 89, j + 47, (int) (partialTick * 100));
-        guiGraphics.setColor(1, 1, 1, 1);
+        guiGraphics.setColor(1f, 1f, 1f, 1);
+        final int maskColor = 0x55777777;
+        ItemStack repairMaterialSlotsItem = this.menu.getSlot(1).getItem();
+        ItemStack resultMaterialSlotsItem = this.menu.getSlot(3).getItem();
+        if (repairMaterialSlotsItem.isEmpty() && resultMaterialSlotsItem.isEmpty()) {
+            guiGraphics.renderItem(
+                RoyalGrindstoneMenu.DEFAULT_REPAIR_MATERIAL.getDefaultInstance(), i + 89, j + 22, (int) (partialTick * 100));
+            guiGraphics.renderItem(
+                RoyalGrindstoneMenu.REPAIR_COST_RECIPES.get(RoyalGrindstoneMenu.DEFAULT_REPAIR_MATERIAL).getSecond().getDefaultInstance(), i + 89, j + 47, (int) (partialTick * 100));
+            guiGraphics.fill(RenderType.guiOverlay(), i + 89, j + 22, i + 89 + 16, j + 22 + 16, maskColor);
+            guiGraphics.fill(RenderType.guiOverlay(), i + 89, j + 47, i + 89 + 16, j + 47 + 16, maskColor);
+        } else if (resultMaterialSlotsItem.isEmpty()) {
+            guiGraphics.renderItem(
+                RoyalGrindstoneMenu.REPAIR_COST_RECIPES.get(repairMaterialSlotsItem.getItem()).getSecond().getDefaultInstance(), i + 89, j + 47, (int) (partialTick * 100));
+            guiGraphics.fill(RenderType.guiOverlay(), i + 89, j + 47, i + 89 + 16, j + 47 + 16, maskColor);
+        } else if (repairMaterialSlotsItem.isEmpty()) {
+            Item repairItem = RoyalGrindstoneMenu.REPAIR_COST_RECIPES.entrySet().stream()
+                .filter(entry -> entry.getValue().getSecond() == resultMaterialSlotsItem.getItem())
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(RoyalGrindstoneMenu.DEFAULT_REPAIR_MATERIAL);
+            guiGraphics.renderItem(repairItem.getDefaultInstance(), i + 89, j + 22, (int) (partialTick * 100));
+            guiGraphics.fill(RenderType.guiOverlay(), i + 89, j + 22, i + 89 + 16, j + 22 + 16, maskColor);
+        }
     }
 
     protected void renderLabels(GuiGraphics guiGraphics) {
