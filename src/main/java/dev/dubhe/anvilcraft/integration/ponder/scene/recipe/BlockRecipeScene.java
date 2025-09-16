a4943bcbd1f3ce08369d0b634775c509a483d616
TBPig <147127248+TBPig@users.noreply.github.com>
2025-09-16 18:25:37 +0800

    手册更新门格海绵

--- src/main/java/dev/dubhe/anvilcraft/integration/ponder/scene/recipe/BlockRecipeScene.java
+++ src/main/java/dev/dubhe/anvilcraft/integration/ponder/scene/recipe/BlockRecipeScene.java
@@ -15,7 +15,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
