00180663d8c321aa88992c11ea16017ce61368e9
burin <ljx33071287@163.com>
2025-11-16 14:00:05 +0800

    style: style check

--- src/main/java/dev/dubhe/anvilcraft/data/lang/OtherLang.java
+++ src/main/java/dev/dubhe/anvilcraft/data/lang/OtherLang.java
@@ -48,6 +48,6 @@ public class OtherLang {
         provider.add("effect.anvilcraft.rage", "Rage");
 
         provider.add("item.anvilcraft.pill.tooltip",
-        "Pills made together with potion to achieve corresponding effects, can be taken quickly");
+            "Pills made together with potion to achieve corresponding effects, can be taken quickly");
     }
 }
