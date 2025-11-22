b2005d2294d6c428c437e325163bbe1099c46627
Phoupraw <467934387@qq.com>
2025-11-22 23:03:18 +0800

    fix #2631

--- src/main/java/dev/dubhe/anvilcraft/item/AmethystAxeItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/AmethystAxeItem.java
@@ -1,22 +1,34 @@
 package dev.dubhe.anvilcraft.item;
 
 import dev.dubhe.anvilcraft.init.enchantment.ModEnchantments;
-import net.minecraft.core.component.DataComponents;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMaps;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.UnmodifiableView;
 
-public class AmethystAxeItem extends AxeItem {
+import java.util.List;
+
+public class AmethystAxeItem extends AxeItem implements IntrinsicEnchantedItem {
     public AmethystAxeItem(Properties properties) {
-        super(ModTiers.AMETHYST,
-            properties.attributes(AxeItem.createAttributes(ModTiers.AMETHYST, 7, -3.2f)));
+        super(ModTiers.AMETHYST, properties.attributes(DiggerItem.createAttributes(ModTiers.AMETHYST, 7, -3.2f)));
     }
 
     @Override
-    public void onCraftedPostProcess(ItemStack stack, Level level) {
-        ItemEnchantments.Mutable enchantments = new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
-        enchantments.set(level.registryAccess().holderOrThrow(ModEnchantments.FELLING_KEY), 1);
-        stack.set(DataComponents.ENCHANTMENTS, enchantments.toImmutable());
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> lines, TooltipFlag flag) {
+        super.appendHoverText(stack, context, lines, flag);
+        IntrinsicEnchantedItem.appendTooltip(stack, context, lines, flag);
+    }
+
+    @ApiStatus.OverrideOnly
+    @Override
+    public @UnmodifiableView Object2IntMap<ResourceKey<Enchantment>> intrinsicEnchantments(ItemStack stack) {
+        return Object2IntMaps.singleton(ModEnchantments.FELLING_KEY, 1);
     }
 }
--- src/main/java/dev/dubhe/anvilcraft/item/AmethystSwordItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/AmethystSwordItem.java
@@ -1,21 +1,33 @@
 package dev.dubhe.anvilcraft.item;
 
 import dev.dubhe.anvilcraft.init.enchantment.ModEnchantments;
-import net.minecraft.core.component.DataComponents;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMaps;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.UnmodifiableView;
 
-public class AmethystSwordItem extends SwordItem {
+import java.util.List;
+
+public class AmethystSwordItem extends SwordItem implements IntrinsicEnchantedItem {
     public AmethystSwordItem(Properties properties) {
         super(ModTiers.AMETHYST, properties.attributes(SwordItem.createAttributes(ModTiers.AMETHYST, 3, -2.4f)));
     }
 
     @Override
-    public void onCraftedPostProcess(ItemStack stack, Level level) {
-        ItemEnchantments.Mutable enchantments = new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
-        enchantments.set(level.registryAccess().holderOrThrow(ModEnchantments.BEHEADING_KEY), 1);
-        stack.set(DataComponents.ENCHANTMENTS, enchantments.toImmutable());
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> lines, TooltipFlag flag) {
+        super.appendHoverText(stack, context, lines, flag);
+        IntrinsicEnchantedItem.appendTooltip(stack, context, lines, flag);
+    }
+
+    @ApiStatus.OverrideOnly
+    @Override
+    public @UnmodifiableView Object2IntMap<ResourceKey<Enchantment>> intrinsicEnchantments(ItemStack stack) {
+        return Object2IntMaps.singleton(ModEnchantments.BEHEADING_KEY, 1);
     }
 }
