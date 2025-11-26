7121e361e91b207d55bfc077e0ba3d1904c87a91
WhereisFff <a181084170@gmail.com>
2025-11-26 23:59:56 +0800

    这次肯定修好了吧

--- src/main/java/dev/dubhe/anvilcraft/client/renderer/entity/model/CauldronOutletModel.java
+++ src/main/java/dev/dubhe/anvilcraft/client/renderer/entity/model/CauldronOutletModel.java
@@ -2,9 +2,9 @@ package dev.dubhe.anvilcraft.client.renderer.entity.model;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
