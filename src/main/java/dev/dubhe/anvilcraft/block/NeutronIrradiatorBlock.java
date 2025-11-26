9eea220a2fb3a4973f846a71fd8c829de68a55dd
WhereisFff <a181084170@gmail.com>
2025-11-26 23:35:17 +0800

    实现中子辐照器对实体的重力加速部分，范围内： 0.无法跳跃 1.下落方块瞬间落地，fallDistance*10 2.漂浮粉上浮3倍速度 3.下落的实体3倍速度，fallDistance*3 4.上升的实体1/3速度

--- src/main/java/dev/dubhe/anvilcraft/block/NeutronIrradiatorBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/NeutronIrradiatorBlock.java
@@ -1,16 +1,23 @@
 package dev.dubhe.anvilcraft.block;
 
 import dev.dubhe.anvilcraft.api.hammer.IHammerRemovable;
+import dev.dubhe.anvilcraft.block.entity.NeutronIrradiatorBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class NeutronIrradiatorBlock extends Block implements IHammerRemovable {
+public class NeutronIrradiatorBlock extends Block implements IHammerRemovable, EntityBlock {
     public static VoxelShape MODEL = Shapes.or(
         Block.box(0, 0, 0, 16, 10, 16),
         Block.box(13, 10, 0, 16, 12, 3),
@@ -43,5 +50,21 @@ public class NeutronIrradiatorBlock extends Block implements IHammerRemovable {
 
     public NeutronIrradiatorBlock(Properties properties) {
         super(properties);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
+        return new NeutronIrradiatorBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> blockEntityType) {
+        return (level1, blockPos, blockState, blockEntity) -> {
+            if (blockEntity instanceof NeutronIrradiatorBlockEntity neutronIrradiatorBlockEntity) {
+                NeutronIrradiatorBlockEntity.tick(level1, blockPos, blockState, neutronIrradiatorBlockEntity);
+            }
+        };
     }
 }
