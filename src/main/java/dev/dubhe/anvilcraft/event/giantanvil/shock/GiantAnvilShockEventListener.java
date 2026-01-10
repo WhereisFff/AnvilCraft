aa267752238d52192705ae095b1a3b88db0cb28d
Fff <a181084170@gmail.com>
2026-01-10 14:56:21 +0800

    让撼地弹射模式可以弹起幻灵铁砧，让附魔台对不同材料有更大概率出特定附魔

--- src/main/java/dev/dubhe/anvilcraft/event/giantanvil/shock/GiantAnvilShockEventListener.java
+++ src/main/java/dev/dubhe/anvilcraft/event/giantanvil/shock/GiantAnvilShockEventListener.java
@@ -4,6 +4,7 @@ import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.behavior.BehaviorTree;
 import dev.dubhe.anvilcraft.api.behavior.TreeNode;
 import dev.dubhe.anvilcraft.api.event.AnvilEvent;
+import dev.dubhe.anvilcraft.entity.FallingSpectralBlockEntity;
 import dev.dubhe.anvilcraft.init.block.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
@@ -112,7 +113,10 @@ public class GiantAnvilShockEventListener {
                 Level level = it.unwrap().level();
                 for (BlockPos pos : it.unwrap().rangePosList()) {
                     BlockState state = level.getBlockState(pos);
-                    if (state.getBlock() instanceof AnvilBlock) {
+                    if (state.is(ModBlocks.SPECTRAL_ANVIL.get())) {
+                        FallingSpectralBlockEntity entity = FallingSpectralBlockEntity.fall(level, pos, state, false, true);
+                        entity.setDeltaMovement(0, 0.31, 0);
+                    } else if (state.getBlock() instanceof AnvilBlock) {
                         FallingBlockEntity entity = new FallingBlockEntity(
                             level,
                             pos.getX() + 0.5,
