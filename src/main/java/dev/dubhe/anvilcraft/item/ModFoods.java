d0c8f74da964bd02d5f394d118ecf163afc44ad9
TBPig <147127248+TBPig@users.noreply.github.com>
2026-01-18 17:01:43 +0800

    巧克力buff增强

--- src/main/java/dev/dubhe/anvilcraft/item/ModFoods.java
+++ src/main/java/dev/dubhe/anvilcraft/item/ModFoods.java
@@ -11,20 +11,23 @@ public class ModFoods {
         .saturationModifier(4.0f)
         .alwaysEdible()
         .fast()
+        .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 3), 1.0f)
         .build();
     public static final FoodProperties CHOCOLATE_BLACK = new FoodProperties.Builder()
         .nutrition(2)
         .saturationModifier(2.0f)
         .alwaysEdible()
         .fast()
-        .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 600, 1), 1.0f)
+        .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 1), 1.0f)
+        .effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 600, 2), 1.0f)
         .build();
     public static final FoodProperties CHOCOLATE_WHITE = new FoodProperties.Builder()
         .nutrition(2)
         .saturationModifier(2.0f)
         .alwaysEdible()
         .fast()
-        .effect(new MobEffectInstance(MobEffects.JUMP, 600, 1), 1.0f)
+        .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 600, 1), 1.0f)
+        .effect(() -> new MobEffectInstance(MobEffects.JUMP, 600, 3), 1.0f)
         .build();
     public static final FoodProperties CREAMY_BREAD_ROLL =
         new FoodProperties.Builder().nutrition(8).saturationModifier(0.5f).build();
