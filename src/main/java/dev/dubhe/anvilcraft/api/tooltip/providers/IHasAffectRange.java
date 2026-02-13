355c4b5aaa237c5ef15f92f1a4dc457db458976d
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2026-02-13 12:29:07 +0800

    feat(InductionLight): 为非默认模式的电感灯增加作用范围显示
    - 为非默认模式的电感灯增加了作用范围显示
    - 改进了部分代码格式

--- src/main/java/dev/dubhe/anvilcraft/api/tooltip/providers/IHasAffectRange.java
+++ src/main/java/dev/dubhe/anvilcraft/api/tooltip/providers/IHasAffectRange.java
@@ -2,9 +2,12 @@ package dev.dubhe.anvilcraft.api.tooltip.providers;
 
 import net.minecraft.world.phys.AABB;
 
+import javax.annotation.Nullable;
+
 /**
  * 拥有作用范围的方块实体
  */
 public interface IHasAffectRange {
+    @Nullable
     AABB shape();
 }
--- src/main/java/dev/dubhe/anvilcraft/block/entity/InductionLightBlockEntity.java
+++ src/main/java/dev/dubhe/anvilcraft/block/entity/InductionLightBlockEntity.java
@@ -1,13 +1,14 @@
 package dev.dubhe.anvilcraft.block.entity;
 
+import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.RipeningManager;
 import dev.dubhe.anvilcraft.api.SpawningManager;
 import dev.dubhe.anvilcraft.api.power.IPowerConsumer;
 import dev.dubhe.anvilcraft.api.power.PowerGrid;
+import dev.dubhe.anvilcraft.api.tooltip.providers.IHasAffectRange;
 import dev.dubhe.anvilcraft.block.InductionLightBlock;
-import dev.dubhe.anvilcraft.block.state.LightColor;
 import dev.dubhe.anvilcraft.util.AabbUtil;
-import dev.dubhe.anvilcraft.util.Lazy;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.core.BlockPos;
@@ -21,10 +22,12 @@ import org.jetbrains.annotations.Nullable;
 
 @Setter
 @Getter
-public class InductionLightBlockEntity extends BlockEntity implements IPowerConsumer {
-    public final Lazy<AABB> blockingArea = new Lazy<>(() -> AabbUtil.centerSectionTo3x3x3(this.getBlockPos()));
+public class InductionLightBlockEntity extends BlockEntity implements IPowerConsumer, IHasAffectRange {
+    @Getter(AccessLevel.NONE)
+    private int ripeningRangeCache = AnvilCraft.CONFIG.inductionLightBlockRipeningRange;
+    private AABB ripeningArea;
+    private AABB blockingArea;
     private PowerGrid grid;
-    private int rangeSize = 5;
 
     public InductionLightBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
@@ -38,35 +41,30 @@ public class InductionLightBlockEntity extends BlockEntity implements IPowerCons
         return new InductionLightBlockEntity(type, pos, blockState);
     }
 
-    public void tick(Level level1) {
-        flushState(level1, getBlockPos());
-        LightColor color = getBlockState().getValue(InductionLightBlock.COLOR);
-        if (color == LightColor.PINK && InductionLightBlock.isLit(this.getBlockState())) {
-            RipeningManager.from(level1).doRipen(getBlockPos());
-        } else if (color == LightColor.YELLOW) {
-            if (level != null) {
-                SpawningManager.addLightBlock(getBlockPos(), level, false);
-            }
-        } else if (color == LightColor.DARK) {
-            if (level != null) {
-                SpawningManager.addLightBlock(getBlockPos(), level, true);
-            }
+    public void tick(Level level) {
+        this.flushState(level, this.getBlockPos());
+        if (InductionLightBlock.canCropGrow(this.getBlockState()) && InductionLightBlock.isLit(this.getBlockState())) {
+            RipeningManager.from(level).doRipen(this.getBlockPos());
+        } else if (InductionLightBlock.canBlockMobSummoning(this.getBlockState())) {
+            SpawningManager.addLightBlock(this.getBlockPos(), level, false);
+        } else if (InductionLightBlock.canBlockAnimalSummoning(this.getBlockState())) {
+            SpawningManager.addLightBlock(this.getBlockPos(), level, true);
         }
     }
 
     @Override
     public int getInputPower() {
-        if (level == null) return 1;
-        return getBlockState().getValue(InductionLightBlock.POWERED)
+        if (this.level == null) return 1;
+        return this.getBlockState().getValue(InductionLightBlock.POWERED)
             ? 0
-            : getBlockState().getValue(InductionLightBlock.COLOR).dissipation;
+            : this.getBlockState().getValue(InductionLightBlock.COLOR).dissipation;
     }
 
     @Override
     @Nullable
     public Level getCurrentLevel() {
-        if (level != null) {
-            return level;
+        if (this.level != null) {
+            return this.level;
         } else {
             return null;
         }
@@ -74,10 +72,39 @@ public class InductionLightBlockEntity extends BlockEntity implements IPowerCons
 
     @Override
     public BlockPos getPos() {
-        return getBlockPos();
+        return this.getBlockPos();
+    }
+
+    @Override
+    public @Nullable AABB shape() {
+        return switch (this.getBlockState().getValue(InductionLightBlock.COLOR)) {
+            case PRIMARY -> null;
+            case PINK -> this.getRipeningArea();
+            case YELLOW, DARK -> this.getBlockingArea();
+        };
+    }
+
+    public AABB getRipeningArea() {
+        if (this.ripeningRangeCache != AnvilCraft.CONFIG.inductionLightBlockRipeningRange) {
+            this.ripeningRangeCache = AnvilCraft.CONFIG.inductionLightBlockRipeningRange;
+        }
+        if (this.ripeningArea == null) {
+            this.ripeningArea = AABB.ofSize(
+                this.getPos().getCenter(),
+                this.ripeningRangeCache,
+                this.ripeningRangeCache,
+                this.ripeningRangeCache
+            );
+        }
+        return this.ripeningArea;
+    }
+
+    public AABB getBlockingArea() {
+        if (this.blockingArea == null) this.blockingArea = AabbUtil.centerSectionTo3x3x3(this.getBlockPos());
+        return this.blockingArea;
     }
 
     public boolean isInRange(Vec3 pos) {
-        return this.blockingArea.get().contains(pos);
+        return this.getBlockingArea().contains(pos);
     }
 }
