140273ca413cb0250e77b67d40e39f506c6aadf5
YocyCraft <hanhengrui@163.com>
2026-02-16 13:52:13 +0800

    fix(item): exp gem is not consumed when used by creative mode player

--- src/main/java/dev/dubhe/anvilcraft/item/ExpGemItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/ExpGemItem.java
@@ -2,6 +2,7 @@ package dev.dubhe.anvilcraft.item;
 
 import com.google.common.collect.Lists;
 import dev.dubhe.anvilcraft.block.ExpFluidBlock;
+import dev.dubhe.anvilcraft.mixin.accessor.VillagerAccessor;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
@@ -41,7 +42,7 @@ public class ExpGemItem extends Item {
         ItemStack itemStack = player.getItemInHand(usedHand);
         int count = player.isShiftKeyDown() ? itemStack.getCount() : 1;
         player.giveExperiencePoints(ExpFluidBlock.XP_POINTS * count);
-        itemStack.shrink(count);
+        itemStack.consume(count, player);
         player.getCooldowns().addCooldown(this, 5);
         return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
     }
@@ -60,11 +61,11 @@ public class ExpGemItem extends Item {
             if (!VillagerData.canLevelUp(villagerLevel)) return InteractionResult.PASS;
 
             updateVillager(villager);
-            stack.shrink(1);
+            stack.consume(1, player);
             return InteractionResult.SUCCESS;
         } else {
             villager.ageUp(AGE_ADDITION, true);
-            stack.shrink(1);
+            stack.consume(1, player);
             return InteractionResult.SUCCESS;
         }
     }
@@ -74,40 +75,48 @@ public class ExpGemItem extends Item {
         int villagerLevel = villagerData.getLevel();
         int villagerXp = villager.getVillagerXp() + VILLAGER_XP;
         villager.setVillagerXp(villagerXp);
-        // 检测经验值是否足够，足够则升级（我好想直接用Villager::increaseMerchantCareer啊，可惜protect）
-        if (villagerXp >= VillagerData.getMaxXpPerLevel(villagerLevel)) {
-            villager.setVillagerXp(villagerXp);
-            ++villagerLevel;
-            villagerData = villagerData.setLevel(villagerLevel);
-            villager.setVillagerData(villagerData);
 
-            villager.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 200, 0));
-            villager.playSound(SoundEvents.PLAYER_LEVELUP, 1.0F, 0.9f + 0.2f * villager.getRandom().nextFloat());
-
-            // 获取对应职业的交易列表
-            Int2ObjectMap<VillagerTrades.ItemListing[]> trades = VillagerTrades.TRADES.get(villagerData.getProfession());
-            if (villager.level().enabledFeatures().contains(FeatureFlags.TRADE_REBALANCE)) {
-                Int2ObjectMap<VillagerTrades.ItemListing[]> rebalanceTrades =
-                    VillagerTrades.EXPERIMENTAL_TRADES.get(villagerData.getProfession());
-                if (rebalanceTrades != null) {
-                    trades = rebalanceTrades;
-                }
-            }
-            if (trades == null || trades.isEmpty()) return;
-            VillagerTrades.ItemListing[] itemlisting = trades.get(villagerLevel);
-            if (itemlisting == null) return;
-            ArrayList<VillagerTrades.ItemListing> arraylist = Lists.newArrayList(itemlisting);
-
-            // 更新村民当前的交易列表（MerchantOffers）
-            MerchantOffers merchantoffers = villager.getOffers();
-            for (int i = 0; i < itemlisting.length; i++) {
-                if (arraylist.isEmpty() || i >= 2) break;
-                MerchantOffer merchantoffer = arraylist.remove(villager.getRandom().nextInt(arraylist.size()))
-                    .getOffer(villager, villager.getRandom());
-                if (merchantoffer != null) {
-                    merchantoffers.add(merchantoffer);
-                }
+        if (villager instanceof VillagerAccessor villager1) {
+            if (villager1.invokeShouldIncreaseLevel()) {
+                villager1.setUpdateMerchantTimer(40);
+                villager1.setIncreaseProfessionLevelOnUpdate(true);
             }
         }
+
+//        // 检测经验值是否足够，足够则升级（我好想直接用Villager::increaseMerchantCareer啊，可惜protect）
+//        if (villagerXp >= VillagerData.getMaxXpPerLevel(villagerLevel)) {
+//            villager.setVillagerXp(villagerXp);
+//            ++villagerLevel;
+//            villagerData = villagerData.setLevel(villagerLevel);
+//            villager.setVillagerData(villagerData);
+//
+//            villager.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 200, 0));
+//            villager.playSound(SoundEvents.PLAYER_LEVELUP, 1.0F, 0.9f + 0.2f * villager.getRandom().nextFloat());
+//
+//            // 获取对应职业的交易列表
+//            Int2ObjectMap<VillagerTrades.ItemListing[]> trades = VillagerTrades.TRADES.get(villagerData.getProfession());
+//            if (villager.level().enabledFeatures().contains(FeatureFlags.TRADE_REBALANCE)) {
+//                Int2ObjectMap<VillagerTrades.ItemListing[]> rebalanceTrades =
+//                    VillagerTrades.EXPERIMENTAL_TRADES.get(villagerData.getProfession());
+//                if (rebalanceTrades != null) {
+//                    trades = rebalanceTrades;
+//                }
+//            }
+//            if (trades == null || trades.isEmpty()) return;
+//            VillagerTrades.ItemListing[] itemlisting = trades.get(villagerLevel);
+//            if (itemlisting == null) return;
+//            ArrayList<VillagerTrades.ItemListing> arraylist = Lists.newArrayList(itemlisting);
+//
+//            // 更新村民当前的交易列表（MerchantOffers）
+//            MerchantOffers merchantoffers = villager.getOffers();
+//            for (int i = 0; i < itemlisting.length; i++) {
+//                if (arraylist.isEmpty() || i >= 2) break;
+//                MerchantOffer merchantoffer = arraylist.remove(villager.getRandom().nextInt(arraylist.size()))
+//                    .getOffer(villager, villager.getRandom());
+//                if (merchantoffer != null) {
+//                    merchantoffers.add(merchantoffer);
+//                }
+//            }
+//        }
     }
 }
