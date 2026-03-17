c82476a3bacf645465d2aed56ec5a5d9e8f37cc5
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2026-03-18 07:12:36 +0800

    fix: 恢复误删除的两个StreamCodec

--- src/main/java/dev/dubhe/anvilcraft/api/power/PowerComponentInfo.java
+++ src/main/java/dev/dubhe/anvilcraft/api/power/PowerComponentInfo.java
@@ -2,17 +2,59 @@ package dev.dubhe.anvilcraft.api.power;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import dev.anvilcraft.lib.v2.recipe.util.CodecUtil;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 
 public record PowerComponentInfo(
-    BlockPos pos, int consumes, int produces, int stores, int capacity, int range, PowerComponentType type) {
+    BlockPos pos,
+    int consumes,
+    int produces,
+    int stores,
+    int capacity,
+    int range,
+    PowerComponentType type
+) {
     public static final Codec<PowerComponentInfo> CODEC = RecordCodecBuilder.create(ins -> ins.group(
-            BlockPos.CODEC.fieldOf("pos").forGetter(o -> o.pos),
-            Codec.INT.fieldOf("consumes").forGetter(o -> o.consumes),
-            Codec.INT.fieldOf("produces").forGetter(o -> o.produces),
-            Codec.INT.fieldOf("stores").forGetter(o -> o.stores),
-            Codec.INT.fieldOf("capacity").forGetter(o -> o.capacity),
-            Codec.INT.fieldOf("range").forGetter(o -> o.range),
-            PowerComponentType.CODEC.fieldOf("type").forGetter(o -> o.type))
-        .apply(ins, PowerComponentInfo::new));
+        BlockPos.CODEC
+            .fieldOf("pos")
+            .forGetter(PowerComponentInfo::pos),
+        Codec.INT
+            .fieldOf("consumes")
+            .forGetter(PowerComponentInfo::consumes),
+        Codec.INT
+            .fieldOf("produces")
+            .forGetter(PowerComponentInfo::produces),
+        Codec.INT
+            .fieldOf("stores")
+            .forGetter(PowerComponentInfo::stores),
+        Codec.INT
+            .fieldOf("capacity")
+            .forGetter(PowerComponentInfo::capacity),
+        Codec.INT
+            .fieldOf("range")
+            .forGetter(PowerComponentInfo::range),
+        PowerComponentType.CODEC
+            .fieldOf("type")
+            .forGetter(PowerComponentInfo::type)
+    ).apply(ins, PowerComponentInfo::new));
+    public static final StreamCodec<ByteBuf, PowerComponentInfo> STREAM_CODEC = CodecUtil.composite(
+        BlockPos.STREAM_CODEC,
+        PowerComponentInfo::pos,
+        ByteBufCodecs.VAR_INT,
+        PowerComponentInfo::consumes,
+        ByteBufCodecs.VAR_INT,
+        PowerComponentInfo::consumes,
+        ByteBufCodecs.VAR_INT,
+        PowerComponentInfo::consumes,
+        ByteBufCodecs.VAR_INT,
+        PowerComponentInfo::consumes,
+        ByteBufCodecs.VAR_INT,
+        PowerComponentInfo::consumes,
+        CodecUtil.enumStreamCodec(PowerComponentType.class),
+        PowerComponentInfo::type,
+        PowerComponentInfo::new
+    );
 }
